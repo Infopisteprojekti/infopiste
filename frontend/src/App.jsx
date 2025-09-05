9f6f9b5a26c21d13d8d723fa069f5048d9e77ce3
simo <49877847+saimouu@users.noreply.github.com>
2025-09-05 11:42:20 +0300

    Add test floorplan as a react component. Delete unused svg-files created by vite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,46 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Floorplan from "./assets/exactum-3.svg?react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [data, setData] = useState(null);
-
-  const apiCall = async () => {
-    const response = await fetch('http://localhost:1234');
-    const text = await response.text();
-    setData(text);
-  };
+  // const [count, setCount] = useState(0);
+  // const [data, setData] = useState(null);
+  //
+  // const apiCall = async () => {
+  //   const response = await fetch('http://localhost:1234');
+  //   const text = await response.text();
+  //   setData(text);
+  // };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Floorplan />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick={apiCall}>
-          Fetch from server
-        </button>
-        <p>{data ? `Server responds with ${data}` : ''}</p>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <div className="card"> */}
+      {/*   <button onClick={apiCall}> */}
+      {/*     Fetch from server */}
+      {/*   </button> */}
+      {/*   <p>{data ? `Server responds with ${data}` : ''}</p> */}
+      {/* </div> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;

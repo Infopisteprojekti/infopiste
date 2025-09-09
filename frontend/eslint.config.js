45d128acddf6ac095b804beed0cac06fb1db8f3c
shdewz <shdewz@gmail.com>
2025-09-09 12:11:42 +0300

    Add semicolon and quote rules to eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -24,6 +24,8 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'semi': ['error', 'always'],
+      'quotes': ['warn', 'single'],
     },
   },
 ])

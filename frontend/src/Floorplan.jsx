3b24ea68ccab3d403330dbf8b72f46f273d8c946
shdewz <shdewz@gmail.com>
2025-09-17 17:27:35 +0300

    Add interactivity to floorplan

---
+++ frontend/src/Floorplan.jsx
@@ -0,0 +1,43 @@
+import { useRef, useEffect } from 'react';
+import FloorplanSVG from './assets/exactum-3.svg?react';
+import './css/Floorplan.css';
+
+const Floorplan = () => {
+    const floorplanRef = useRef(null);
+
+    useEffect(() => {
+        const floorplan = floorplanRef.current;
+        if (floorplan) {
+            const rooms = floorplan.querySelectorAll('g');
+            for (const room of rooms) {
+                const child = room.querySelector('*');
+                const roomId = child?.id;
+                if (roomId) {
+                    child.classList.add('room');
+
+                    const handler = () => {
+                        const status = getRoomStatus(roomId);
+                        alert(`Room ${roomId} status: ${status}`);
+                    };
+
+                    room.addEventListener('click', handler);
+                    room._clickHandler = handler;
+                }
+            }
+
+            return () => {
+                for (const room of rooms) {
+                    room.removeEventListener('click', room._clickHandler);
+                }
+            };
+        }
+    });
+
+    return <FloorplanSVG ref={floorplanRef}></FloorplanSVG>
+};
+
+const getRoomStatus = room => {
+    return 'unknown';
+};
+
+export default Floorplan;

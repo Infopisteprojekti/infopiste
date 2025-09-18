5a570bcd7dc5b10ada446fcbed673db09e2d6a41
candyliaa <astronomy.dev@gmail.com>
2025-09-18 16:32:26 +0300

    added test for alert after clicking on room

--- frontend/tests/Floorplan.test.jsx
+++ frontend/tests/Floorplan.test.jsx
@@ -15,4 +15,16 @@ describe("Floorplan", () => {
         const room = container.querySelector("[data-room-id='A346']")
         expect(room).toBeInTheDocument()
     })
+
+    test("room status is shown when room is clicked", async () => {
+        const alertMock = vi.spyOn(window, "alert").mockImplementation(() => { })
+        const user = userEvent.setup()
+
+        const room = container.querySelector("[data-room-id='A346']")
+
+        await user.click(room)
+
+        expect(alertMock).toBeCalled()
+        alertMock.mockRestore()
+    })
 })

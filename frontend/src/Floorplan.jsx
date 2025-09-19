064f974746965e89b8c9a55a7308747899d09fa1
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2025-09-19 14:52:59 +0300

    Add button to pdf view

--- frontend/src/Floorplan.jsx
+++ frontend/src/Floorplan.jsx
@@ -1,9 +1,15 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import FloorplanSVG from './assets/exactum-3.svg?react';
 import './css/Floorplan.css';
+import Pdfview from './Pdfview.jsx';
 
 const Floorplan = () => {
     const floorplanRef = useRef(null);
+    const [active, setactive] = useState(1);
+
+    const handleClick = () => {
+        setactive(!active);
+    };
 
     useEffect(() => {
         const floorplan = floorplanRef.current;
@@ -34,7 +40,10 @@ const Floorplan = () => {
         }
     });
 
-    return <FloorplanSVG ref={floorplanRef}></FloorplanSVG>;
+    return (<>
+            <button onClick={handleClick}>pdfs</button>
+            {active ? <FloorplanSVG ref={floorplanRef}></FloorplanSVG> : <Pdfview />}
+            </>);
 };
 
 const getRoomStatus = () => {
---
+++ frontend/src/Pdfview.jsx
@@ -0,0 +1,8 @@
+
+
+const Pdfview = () => {
+    return <h1>Pdf view</h1>;
+};
+
+
+export default Pdfview;

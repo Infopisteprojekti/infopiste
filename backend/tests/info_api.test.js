4c66fb701df1b9c79886fc30a5e8144f31076e49
candyliaa <astronomy.dev@gmail.com>
2025-09-19 17:37:51 +0300

    room tests added

--- backend/tests/info_api.test.js
+++ backend/tests/info_api.test.js
@@ -10,3 +10,28 @@ test("health check returns ok", async () => {
 
     assert.strictEqual(res.body.status, "ok")
 })
+
+test("rooms are returned", async () => {
+    const res = await api
+        .get("/api/rooms")
+        .expect(200)
+        .expect("Content-Type", /application\/json/)
+
+    assert.strictEqual(Array.isArray(res.body), true, "rooms are in array")
+    assert.ok(res.body.length > 0, "room array is not empty")
+})
+
+test("room with correct id is found", async () => {
+    const res = await api
+        .get("/api/rooms/A310")
+        .expect(200)
+        .expect("Content-Type", /application\/json/)
+    
+    assert.strictEqual(res.body.id, "A310")
+})
+
+test("reservations for specific room are returned", async () => {
+    const res = await api.get("/api/rooms/A310/reservations").expect(200)
+
+    assert.strictEqual(Array.isArray(res.body), true, "reservations are in array")
+})

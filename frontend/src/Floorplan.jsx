c3ca375e191e423c3015e301dea5ae27f0e958eb
simo <49877847+saimouu@users.noreply.github.com>
2025-09-22 12:53:20 +0300

    Add wrapper to make floorplan zoomable (still broken)

--- frontend/src/Floorplan.jsx
+++ frontend/src/Floorplan.jsx
@@ -1,44 +1,53 @@
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 import { useRef, useEffect } from 'react';
 import FloorplanSVG from './assets/exactum-3.svg?react';
 import './css/Floorplan.css';
 
 const Floorplan = () => {
-    const floorplanRef = useRef(null);
+  const floorplanRef = useRef(null);
 
-    useEffect(() => {
-        const floorplan = floorplanRef.current;
-        if (floorplan) {
-            const rooms = floorplan.querySelectorAll('g');
-            for (const room of rooms) {
-                const child = room.querySelector('*');
-                const roomId = child?.id;
-                if (roomId) {
-                    child.classList.add('room');
-                    room.setAttribute('data-room-id', roomId);
+  useEffect(() => {
+    const floorplan = floorplanRef.current;
+    if (floorplan) {
+      const rooms = floorplan.querySelectorAll('g');
+      for (const room of rooms) {
+        const child = room.querySelector('*');
+        const roomId = child?.id;
+        if (roomId) {
+          child.classList.add('room');
+          room.setAttribute('data-room-id', roomId);
 
-                    const handler = () => {
-                        const status = getRoomStatus();
-                        alert(`Room ${roomId} status: ${status}`);
-                    };
+          const handler = () => {
+            const status = getRoomStatus();
+            alert(`Room ${roomId} status: ${status}`);
+          };
 
-                    room.addEventListener('click', handler);
-                    room._clickHandler = handler;
-                }
-            }
-
-            return () => {
-                for (const room of rooms) {
-                    room.removeEventListener('click', room._clickHandler);
-                }
-            };
+          room.addEventListener('click', handler);
+          room._clickHandler = handler;
         }
-    });
+      }
 
-    return <FloorplanSVG ref={floorplanRef}></FloorplanSVG>;
+      return () => {
+        for (const room of rooms) {
+          room.removeEventListener('click', room._clickHandler);
+        }
+      };
+    }
+  });
+
+  // return <FloorplanSVG ref={floorplanRef}></FloorplanSVG>;
+
+  return (
+    <TransformWrapper>
+      <TransformComponent>
+        <FloorplanSVG ref={floorplanRef}></FloorplanSVG>;
+      </TransformComponent>
+    </TransformWrapper>
+  );
 };
 
 const getRoomStatus = () => {
-    return 'unknown';
+  return 'unknown';
 };
 
 export default Floorplan;

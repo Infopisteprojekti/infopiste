351a6f4dfedfefe8d5bb40bac774e5429eef69c0
jipeso <jipesonen@gmail.com>
2025-09-24 11:36:51 +0300

    refactor backend entry point

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+import logger from './utils/logger.js';
+import { MONGO_DB_URL } from './utils/config.js';
+import { requestLogger, unknownEndpoint } from './utils/middleware.js';
+
+import roomsRouter from './controllers/rooms.js';
+
+const app = express();
+
+mongoose.set('strictQuery', false);
+logger.info('Connecting to', MONGO_DB_URL);
+
+mongoose.connect(MONGO_DB_URL)
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch(error => {
+    logger.error('error connection to MongoDB:', error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use(requestLogger);
+
+app.use('/api/rooms', roomsRouter);
+
+app.get('/', (req, res) => res.send('infonäyttö backend'));
+
+app.get('/api/hello', async (req, res) => {
+  res.json({message: 'hello from backend server'});
+});
+
+app.get('/health', (req, res) => res.status(200).json({status: 'ok'}));
+
+app.use(unknownEndpoint);
+
+export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-
+import app from './app.js';
 import logger from './utils/logger.js';
-import { PORT, MONGO_DB_URL } from './utils/config.js';
-import { requestLogger, unknownEndpoint } from './utils/middleware.js';
-
-import roomsRouter from './controllers/rooms.js';
-
-const app = express();
-
-mongoose.set('strictQuery', false);
-logger.info('Connecting to', MONGO_DB_URL);
-
-mongoose.connect(MONGO_DB_URL)
-  .then(() => {
-    logger.info('connected to MongoDB');
-  })
-  .catch(error => {
-    logger.error('error connection to MongoDB:', error.message);
-  });
-
-app.use(cors());
-app.use(express.json());
-app.use(requestLogger);
-
-app.use('/api/rooms', roomsRouter);
-
-app.get('/', (req, res) => res.send('infonäyttö backend'));
-
-app.get('/api/hello', async (req, res) => {
-  res.json({message: 'hello from backend server'});
-});
-
-app.get('/health', (req, res) => res.status(200).json({status: 'ok'}));
+import { PORT } from './utils/config.js';
 
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
 });
-
-app.use(unknownEndpoint);
-
-export default app;

2070c18c40a95c863706e368b85896ba4350d5a4
jipeso <jipesonen@gmail.com>
2025-09-24 15:27:42 +0300

    create routes and organize React components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,31 @@
-import Floorplan from './Floorplan';
+import { Routes, Route, Link } from 'react-router-dom';
+import Floorplan from './components/Floorplan';
+import BulletinBoard from './components/BulletinBoard';
 import './css/reset.css';
 
 function App() {
+
   return (
-    <>
-      <div style={{ marginTop: '40px' }}>
-        <Floorplan />
+    <div>
+      <div>
+        <Link style={{ marginRight: 10} }to="/">Floorplan</Link>
+        <Link to="/board">Bulletin Board</Link>
       </div>
-    </>
+      <div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div style={{ marginTop: 40 }}>
+                <Floorplan />
+              </div>
+            }
+          />
+          <Route path="/board" element={<BulletinBoard />} />
+        </Routes>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/BulletinBoard.jsx
@@ -0,0 +1,10 @@
+const BulletinBoard = () => {
+  return (
+    <div>
+      <br></br>
+      <h1>Files</h1>
+    </div>
+  );
+};
+
+export default BulletinBoard;

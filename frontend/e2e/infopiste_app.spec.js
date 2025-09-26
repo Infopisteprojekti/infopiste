f28658ffc46d42288b9c34a6bec5987ed220b55c
astro <astronomy.dev@gmail.com>
2025-09-26 13:50:40 +0300

    initial tests

---
+++ frontend/e2e/infopiste_app.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Infopiste", () => {
+  test("home page can be opened", async ({ page }) => {
+    await page.goto("http://localhost:5173");
+
+    // There is no proper home page yet - no title or headings.
+    // Once those are added, this should be changed to something more accurate.
+    await expect(page.getByText("A348")).toBeVisible();
+  });
+
+  test("zoom buttons exist", async ({ page }) => {
+    await page.goto("http://localhost:5173");
+
+    await expect(page.getByText("Zoom In")).toBeVisible();
+    await expect(page.getByText("Zoom Out")).toBeVisible();
+    await expect(page.getByText("Reset")).toBeVisible();
+  });
+});

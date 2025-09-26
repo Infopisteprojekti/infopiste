641f49e9a7f1d01e49bdd105ef12c3fb070236ea
astro <astronomy.dev@gmail.com>
2025-09-26 15:46:04 +0300

    add test for clicking zoom buttons

--- frontend/e2e/infopiste_app.spec.js
+++ frontend/e2e/infopiste_app.spec.js
@@ -1,19 +1,27 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test.describe("Infopiste", () => {
-  test("home page can be opened", async ({ page }) => {
-    await page.goto("http://localhost:5173");
+test.describe('Infopiste', () => {
+  test('home page can be opened', async ({ page }) => {
+    await page.goto('http://localhost:5173');
 
     // There is no proper home page yet - no title or headings.
     // Once those are added, this should be changed to something more accurate.
-    await expect(page.getByText("A348")).toBeVisible();
+    await expect(page.getByText('A348')).toBeVisible();
   });
 
-  test("zoom buttons exist", async ({ page }) => {
-    await page.goto("http://localhost:5173");
+  test('zoom buttons exist', async ({ page }) => {
+    await page.goto('http://localhost:5173');
 
-    await expect(page.getByText("Zoom In")).toBeVisible();
-    await expect(page.getByText("Zoom Out")).toBeVisible();
-    await expect(page.getByText("Reset")).toBeVisible();
+    await expect(page.getByText('Zoom In')).toBeVisible();
+    await expect(page.getByText('Zoom Out')).toBeVisible();
+    await expect(page.getByText('Reset')).toBeVisible();
+  });
+
+  test('zoom buttons are clickable', async ({ page }) => {
+    await page.goto('http://localhost:5173');
+
+    await page.getByText('Zoom In').click();
+    await page.getByText('Zoom Out').click();
+    await page.getByText('Reset').click();
   });
 });

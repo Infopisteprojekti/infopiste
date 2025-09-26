90b9be3f2a8c50de5c303f2d41af8c84d403ab8c
jipeso <149601375+jipeso@users.noreply.github.com>
2025-09-26 10:01:14 +0300

    Update frontend/src/main.jsx
    Co-authored-by: shdewz <57327918+shdewz@users.noreply.github.com>

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx';
-import './css/index.css';
 import './css/reset.css';
+import './css/index.css';
 
 
 createRoot(document.getElementById('root')).render(

05781b63ddb05d8adf955abf7bc7304c45705940
candyliaa <astronomy.dev@gmail.com>
2025-09-30 16:23:36 +0300

    add MS_API_KEY to config.js

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -4,3 +4,5 @@ export const MONGO_DB_URL =
   process.env.MONGO_DB_URL === 'test'
     ? process.env.TEST_MONGO_DB_URL
     : process.env.MONGO_DB_URL;
+
+export const MS_API_KEY = process.env.MS_API_KEY;

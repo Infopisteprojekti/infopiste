ded911671e9ea43cf4c2da891014942186c1318e
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2025-10-03 12:37:11 +0300

    qr code template to bulletin board

--- frontend/src/components/BulletinBoard.jsx
+++ frontend/src/components/BulletinBoard.jsx
@@ -1,8 +1,12 @@
+import qrcode from '../assets/QRCode-for-Example-form.png';
+import '../css/BulletinBoard.css';
+
 const BulletinBoard = () => {
   return (
     <div>
       <br />
       <h1>Files</h1>
+      <img src={qrcode} class='bottomright' width="300" height="300"/>
     </div>
   );
 };

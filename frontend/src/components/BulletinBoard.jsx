58db7fb897044163248b57462d25902d402bd11d
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2025-10-03 13:55:15 +0300

    Fixed lint errors

--- frontend/src/components/BulletinBoard.jsx
+++ frontend/src/components/BulletinBoard.jsx
@@ -5,19 +5,19 @@ import '../css/BulletinBoard.css';
 const BulletinBoard = () => {
   return (
     <TransformWrapper initialScale={1} minScale={1} maxScale={5}>
-        <>
-          <div className='bottomright'>
-            <img src={qrcode} width="300" height="300"/>
-          </div>
+      <>
+        <div className="bottomright">
+          <img src={qrcode} width="300" height="300" />
+        </div>
 
-          <TransformComponent
-            wrapperStyle={{ width: '100%', height: '100vh' }}
-            contentStyle={{ width: '100%', height: '100%' }}
-          >
-            <br />
-            <h1>Files</h1>
-          </TransformComponent>
-        </>
+        <TransformComponent
+          wrapperStyle={{ width: '100%', height: '100vh' }}
+          contentStyle={{ width: '100%', height: '100%' }}
+        >
+          <br />
+          <h1>Files</h1>
+        </TransformComponent>
+      </>
     </TransformWrapper>
   );
 };

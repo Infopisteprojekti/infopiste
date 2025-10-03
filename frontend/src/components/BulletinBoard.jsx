4b781ac0c3a685d21115cb82ac9f736062eb9936
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2025-10-03 13:25:52 +0300

    Navigation bar and qr code are visible at all times

--- frontend/src/components/BulletinBoard.jsx
+++ frontend/src/components/BulletinBoard.jsx
@@ -1,13 +1,24 @@
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
 import qrcode from '../assets/QRCode-for-Example-form.png';
 import '../css/BulletinBoard.css';
 
 const BulletinBoard = () => {
   return (
-    <div>
-      <br />
-      <h1>Files</h1>
-      <img src={qrcode} class='bottomright' width="300" height="300"/>
-    </div>
+    <TransformWrapper initialScale={1} minScale={1} maxScale={5}>
+        <>
+          <div className='bottomright'>
+            <img src={qrcode} width="300" height="300"/>
+          </div>
+
+          <TransformComponent
+            wrapperStyle={{ width: '100%', height: '100vh' }}
+            contentStyle={{ width: '100%', height: '100%' }}
+          >
+            <br />
+            <h1>Files</h1>
+          </TransformComponent>
+        </>
+    </TransformWrapper>
   );
 };
 

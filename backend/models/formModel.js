6d06873a76bf9e77d467e436c67d180b5da44776
candyliaa <astronomy.dev@gmail.com>
2025-10-07 16:03:02 +0300

    add models directory with formModel.js

---
+++ backend/models/formModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const formSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    fileUrl: {
+        type: String,
+        required: true
+    }
+})
+
+export const Form = mongoose.model('Form', formSchema);

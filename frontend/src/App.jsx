a425d4bd53c2b89806ccdec1048ccb2723ee04eb
simo <49877847+saimouu@users.noreply.github.com>
2025-10-11 15:24:26 +0300

    Add base language switcher

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import Floorplan from './components/Floorplan';
 import BulletinBoard from './components/BulletinBoard';
+import { ChevronUp } from 'lucide-react';
 
 import { useTranslation } from 'react-i18next';
 
 function App() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = lng => {
+    i18n.changeLanguage(lng);
+  };
 
   return (
     <div>
@@ -16,6 +21,16 @@ function App() {
         <Link className="navbar-button" to="/board">
           {t('navbar.bulletinboard')}
         </Link>
+        <div className="navbar-language-switcher">
+          <select
+            value={i18n.resolvedLanguage}
+            onChange={e => changeLanguage(e.target.value)}
+          >
+            <option value="fi">Finnish</option>
+            <option value="en">English</option>
+            <option value="sv">Swedish</option>
+          </select>
+        </div>
       </div>
       <div>
         <Routes>

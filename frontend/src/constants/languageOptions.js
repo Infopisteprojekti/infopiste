fb176c5d4307e0ede26a618c4d4d980d2b903a8e
simo <49877847+saimouu@users.noreply.github.com>
2025-10-12 18:26:13 +0300

    Add language options to constants

---
+++ frontend/src/constants/languageOptions.js
@@ -0,0 +1,7 @@
+const LANGUAGE_OPTIONS = [
+  { value: 'fi', label: 'FI', fullName: 'Finnish' },
+  { value: 'en', label: 'EN', fullName: 'English' },
+  { value: 'sv', label: 'SV', fullName: 'Swedish' },
+];
+
+export default LANGUAGE_OPTIONS;

9dbbb0c305f64bb03941df2d0b2756e156efd80d
shdewz <shdewz@gmail.com>
2025-10-14 15:26:32 +0300

    Remove css import from tests and apply prettier

--- frontend/tests/Floorplan.test.jsx
+++ frontend/tests/Floorplan.test.jsx
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Floorplan from '../src/components/Floorplan';
-import App from '../src/App'
-import '../src/css/Floorplan.css';
+import App from '../src/App';
 import { describe, expect, test, vi } from 'vitest';
 import userEvent from '@testing-library/user-event';
 
@@ -10,28 +9,28 @@ import i18n from './testSetup.js';
 
 // Mock ResizeObserver
 class ResizeObserver {
-  constructor(callback) { }
-  observe() { }
-  unobserve() { }
-  disconnect() { }
+  constructor(callback) {}
+  observe() {}
+  unobserve() {}
+  disconnect() {}
 }
 
 global.ResizeObserver = ResizeObserver;
 
 // Mock svg asset in tests, click event for A346
-const clickMock = vi.fn()
+const clickMock = vi.fn();
 
 vi.mock('../src/assets/exactum-3.svg?react', () => ({
   default: ({ ref }) => (
     <svg ref={ref}>
-      <g data-room-id='A346' _status='available' onClick={clickMock}>
-        <rect id='A346' />
+      <g data-room-id="A346" _status="available" onClick={clickMock}>
+        <rect id="A346" />
       </g>
-      <g data-room-id='A348' _status='reserved'>
-        <rect id='A348' />
+      <g data-room-id="A348" _status="reserved">
+        <rect id="A348" />
       </g>
-      <g data-room-id='A311' _status='unavailable'>
-        <rect id='A311' />
+      <g data-room-id="A311" _status="unavailable">
+        <rect id="A311" />
       </g>
     </svg>
   ),
@@ -39,61 +38,89 @@ vi.mock('../src/assets/exactum-3.svg?react', () => ({
 
 describe('Floorplan', () => {
   test('floorplan is rendered correctly', async () => {
-    render(<MemoryRouter><Floorplan /></MemoryRouter>);
+    render(
+      <MemoryRouter>
+        <Floorplan />
+      </MemoryRouter>
+    );
 
-    const room = document.querySelector('[data-room-id="A346"]')
+    const room = document.querySelector('[data-room-id="A346"]');
     expect(room).toBeInTheDocument();
   });
 
   test('correct amount of rooms is rendered', () => {
-    render(<MemoryRouter><Floorplan /></MemoryRouter>);
+    render(
+      <MemoryRouter>
+        <Floorplan />
+      </MemoryRouter>
+    );
 
     const rooms = document.querySelectorAll('g[data-room-id]');
     expect(rooms.length).toBe(3);
-  })
+  });
 
   test('zoom buttons exist', () => {
-    render(<MemoryRouter><Floorplan /></MemoryRouter>);
+    render(
+      <MemoryRouter>
+        <Floorplan />
+      </MemoryRouter>
+    );
 
     expect(screen.getByText(/Zoom In/i)).toBeInTheDocument();
     expect(screen.getByText(/Zoom Out/i)).toBeInTheDocument();
     expect(screen.getByText(/Reset/i)).toBeInTheDocument();
-  })
+  });
 
   test('clicking room is possible', async () => {
     const user = userEvent.setup();
 
-    render(<MemoryRouter><Floorplan /></MemoryRouter>);
+    render(
+      <MemoryRouter>
+        <Floorplan />
+      </MemoryRouter>
+    );
 
     const room = document.querySelector('[data-room-id="A346"]');
-    await user.click(room)
+    await user.click(room);
 
     expect(clickMock).toHaveBeenCalled();
-  })
+  });
 
   test('url parameters work', async () => {
-    render(<MemoryRouter initialEntries={['/?floor=2']}><Floorplan /></MemoryRouter>);
+    render(
+      <MemoryRouter initialEntries={['/?floor=2']}>
+        <Floorplan />
+      </MemoryRouter>
+    );
 
-    const room = document.querySelector('[data-room-id="B233"]')
+    const room = document.querySelector('[data-room-id="B233"]');
     expect(room).toBeInTheDocument();
-  })
+  });
 
   test('bulletin board can be accessed', async () => {
     const user = userEvent.setup();
 
-    render(<MemoryRouter><App /></MemoryRouter>);
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
 
     const bulletinBoardButton = await screen.findByText('Bulletin Board');
     await user.click(bulletinBoardButton);
 
     const addFileButton = await screen.findByText('Add file');
     expect(addFileButton).toBeInTheDocument();
-  })
+  });
 
   test('qr code to upload files appears', async () => {
     const user = userEvent.setup();
 
-    render(<MemoryRouter><App /></MemoryRouter>);
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
 
     const bulletinBoardButton = await screen.findByText('Bulletin Board');
     await user.click(bulletinBoardButton);
@@ -103,12 +130,16 @@ describe('Floorplan', () => {
 
     const popup = document.getElementById('popup');
     expect(popup.classList.contains('open-popup')).toBe(true);
-  })
+  });
 
   test('qr code can be closed', async () => {
     const user = userEvent.setup();
 
-    render(<MemoryRouter><App /></MemoryRouter>);
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
 
     const bulletinBoardButton = await screen.findByText('Bulletin Board');
     await user.click(bulletinBoardButton);
@@ -121,13 +152,17 @@ describe('Floorplan', () => {
 
     const popup = document.getElementById('popup');
     expect(popup.classList.contains('open-popup')).toBe(false);
-  })
+  });
 
   test('Language can be changed from English to Finnish', async () => {
     const user = userEvent.setup();
     const spy = vi.spyOn(i18n, 'changeLanguage');
 
-    render(<MemoryRouter><App /></MemoryRouter>);
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
 
     expect(await screen.findByText('Floor Plan')).toBeInTheDocument();
 
@@ -136,9 +171,9 @@ describe('Floorplan', () => {
     await user.click(langDropDown);
 
     const finnishOption = await screen.findByText('suomi');
-    await user.click(finnishOption)
+    await user.click(finnishOption);
 
     expect(spy).toHaveBeenCalledWith('fi');
     expect(await screen.findByText('Kartta')).toBeInTheDocument();
-  })
+  });
 });

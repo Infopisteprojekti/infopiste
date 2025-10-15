df676f5565777acfd7a4cf8de099416cdfb70031
simo <49877847+saimouu@users.noreply.github.com>
2025-10-15 16:15:16 +0300

    Change default lang to Finnish and add ability to set language via query parameter on startup

--- frontend/utils/i18n/index.js
+++ frontend/utils/i18n/index.js
@@ -1,17 +1,29 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LANGUAGE_OPTIONS from '../../src/constants/languageOptions';
 
 import enLang from './locales/en.json';
 import fiLang from './locales/fi.json';
 import svLang from './locales/sv.json';
 
+const DEFAULT_LANG = 'fi';
+
+const urlParams = new URLSearchParams(window.location.search);
+const langQuery = urlParams.get('lang');
+const savedLang = localStorage.getItem('lang');
+
+const lang = langQuery || savedLang || DEFAULT_LANG;
+
+if (lang) {
+  localStorage.setItem('lang', lang);
+}
+
 i18n
   .use(initReactI18next)
 
   .init({
-    // debug: true,
-    lng: 'en',
-    fallbackLng: 'en',
+    lng: lang,
+    fallbackLng: DEFAULT_LANG,
 
     interpolation: {
       escapeValue: false,

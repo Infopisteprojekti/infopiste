3c29f2157e3c5cb89f3d46678fd21c0cd8638415
candyliaa <astronomy.dev@gmail.com>
2025-10-16 15:41:03 +0300

    add endpoint for fetching forms from db

---
+++ backend/controllers/forms.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { Form } from '../models/formModel.js';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const forms = await Form.find();
+        res.json(forms);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'server error' });
+    }
+})
+
+export default router;

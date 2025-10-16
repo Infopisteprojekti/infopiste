c168a072ae3a42102d8ee5801c8fec7d53cf15cb
candyliaa <astronomy.dev@gmail.com>
2025-10-16 20:25:21 +0300

    display pdf file in bulletin board

--- frontend/src/components/BulletinBoard.jsx
+++ frontend/src/components/BulletinBoard.jsx
@@ -3,10 +3,36 @@ import '../styles/components/Button.css';
 import '../styles/components/BulletinBoard.css';
 import { useTranslation } from 'react-i18next';
 import { useState } from 'react';
+import { useEffect } from 'react';
+
+const baseUrl =
+  import.meta.env.VITE_API_BASE_URL ||
+  'https://infopiste-backend-ohtuprojekti-staging.ext.ocp-test-0.k8s.it.helsinki.fi';
 
 const BulletinBoard = () => {
   const { t } = useTranslation();
   const [qrState, setQrState] = useState(false);
+  const [forms, setForms] = useState([]);
+
+  useEffect(() => {
+    const fetchForms = async () => {
+      try {
+        const res = await fetch(`${baseUrl}/api/forms`)
+        if (!res.ok) throw new Error(`Error fetching room data: ${res.status}`)
+
+        const data = await res.json();
+        if (data.error) throw new Error(data.error);
+  
+        setForms(data);
+        return;
+
+      } catch (err) {
+        console.error(err);
+        return null;
+      }
+    }
+    fetchForms()
+  }, []);
 
   const toggleQr = () => {
     const newState = !qrState;
@@ -41,6 +67,23 @@ const BulletinBoard = () => {
 
       <br />
       <h1>Files</h1>
+      <ul>
+        {forms.map(form => (
+          <li key={form._id}>
+            <h3>{form.title}</h3>
+            <p>
+              {new Date(form.startDate).toLocaleDateString()} -{' '}
+              {new Date(form.endDate).toLocaleDateString()}
+            </p>
+            <iframe
+              src={form.fileUrl}
+              width="50%"
+              height="700px"
+              title={form.title}
+            />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };

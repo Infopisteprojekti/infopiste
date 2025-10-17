ce098e8d34ce8a50058d5674164b4dd8da68cfa3
candyliaa <astronomy.dev@gmail.com>
2025-10-17 18:18:03 +0300

    fix lint

--- frontend/e2e/testUtils.js
+++ frontend/e2e/testUtils.js
@@ -16,38 +16,38 @@ export const mockForms = [
 ];
 
 export const mockRooms = [
-          {
-            id: 'A344',
-            type: 'meeting',
-            reservations: [],
-          },
-          {
-            id: 'A345',
-            type: 'meeting',
-            reservations: [
-              {
-                id: 1,
-                subject: 'Best Meeting',
-                organizer: 'Some Person',
-                start: {
-                  dateTime: '1990-01-01T12:00:00',
-                },
-                end: {
-                  dateTime: '2125-01-01T12:00:00',
-                },
-                location: {
-                  displayName: 'Room A345',
-                  locationType: 'confRoom',
-                },
-              },
-            ],
-          },
-          {
-            id: 'A346',
-            type: 'office',
-            reservations: [],
-          },
-        ]
+  {
+    id: 'A344',
+    type: 'meeting',
+    reservations: [],
+  },
+  {
+    id: 'A345',
+    type: 'meeting',
+    reservations: [
+      {
+        id: 1,
+        subject: 'Best Meeting',
+        organizer: 'Some Person',
+        start: {
+          dateTime: '1990-01-01T12:00:00',
+        },
+        end: {
+          dateTime: '2125-01-01T12:00:00',
+        },
+        location: {
+          displayName: 'Room A345',
+          locationType: 'confRoom',
+        },
+      },
+    ],
+  },
+  {
+    id: 'A346',
+    type: 'office',
+    reservations: [],
+  },
+];
 
 export async function mockFormsRoute(page, forms = mockForms) {
   await page.route('**/api/forms', route => {
@@ -61,8 +61,8 @@ export async function mockFormsRoute(page, forms = mockForms) {
 export async function mockRoomsRoute(page, rooms = mockRooms) {
   await page.route('**/api/rooms', route => {
     route.fulfill({
-        contentType: 'application/json',
-        body: JSON.stringify(rooms)
-    })
-  })
+      contentType: 'application/json',
+      body: JSON.stringify(rooms),
+    });
+  });
 }

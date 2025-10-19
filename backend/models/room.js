649b6f0684df119e365e7cf451cf2cc66e95c8df
jipeso <149601375+jipeso@users.noreply.github.com>
2025-10-19 13:46:02 +0300

    add room mongoose model

---
+++ backend/models/room.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const roomSchema = mongoose.Schema({
+  roomEmail: String,
+  displayName: String,
+  capacity: Number,
+  floorNumber: Number,
+  isWheelChairAccessible: Boolean,
+  tags: [String],
+});
+
+roomSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+export default mongoose.model('Room', roomSchema);

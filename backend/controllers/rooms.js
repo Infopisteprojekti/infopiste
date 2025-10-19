6a55273240451582b5657c3abec668a159477931
jipeso <149601375+jipeso@users.noreply.github.com>
2025-10-19 14:52:40 +0300

    add route for all rooms with database

--- backend/controllers/rooms.js
+++ backend/controllers/rooms.js
@@ -4,6 +4,7 @@ import {
   fetchReservationsById,
 } from '../services/rooms.js';
 import redis from '../utils/redisClient.js';
+import Room from '../models/room.js';
 import { TTL_SECONDS } from '../utils/config.js';
 
 const router = Router();
@@ -58,6 +59,34 @@ router.get('/:id/reservations', async (request, response) => {
     response.status(500).json({
       error: `Failed to fetch reservations for room: ${id}, error: ${err}`,
     });
+  }
+});
+
+router.get('/allRooms', async (request, response) => {
+  const cacheKey = 'rooms:all';
+
+  try {
+    const cached = await redis.get(cacheKey);
+    if (cached) {
+      return response.status(200).json({
+        source: 'cache',
+        data: JSON.parse(cached),
+      });
+    }
+
+    const rooms = await Room.find({});
+
+    if (rooms.length > 0) {
+      await redis.set(cacheKey, JSON.stringify(rooms));
+      await redis.expire(cacheKey, TTL_SECONDS);
+    }
+
+    response.status(200).json({
+      source: 'database',
+      data: rooms,
+    });
+  } catch (err) {
+    response.status(500).json({ error: err });
   }
 });
 

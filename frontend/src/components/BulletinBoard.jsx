951a6a67d2c56fd3db3e05ccc92299f761d5ad89
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2025-10-26 13:19:04 +0200

    Fixed lint

--- frontend/src/components/BulletinBoard.jsx
+++ frontend/src/components/BulletinBoard.jsx
@@ -93,7 +93,9 @@ const BulletinBoard = () => {
   return (
     <div>
       <button onClick={RotatePdfs} className="button">
-        {automaticRotation ? t('bulletinboard.stop-rotation') : t('bulletinboard.continue-rotation')}
+        {automaticRotation
+          ? t('bulletinboard.stop-rotation')
+          : t('bulletinboard.continue-rotation')}
       </button>
       <button
         type="submit"

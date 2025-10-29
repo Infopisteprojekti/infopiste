a6ec02bc271bbd03a2e09fd90706ffd70cb4523f
jipeso <149601375+jipeso@users.noreply.github.com>
2025-10-29 18:46:06 +0200

    remove unused mockdata from backend

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import app from './app.js';
 import logger from './utils/logger.js';
 import { PORT } from './utils/config.js';
 import { connectToDatabase } from './utils/dbConnection.js';
-import { insertMockData } from './mockdata/mock-forms-in-db.js';
 import redis from './utils/redisClient.js';
 import graphClient from './utils/graphClient.js';
 
@@ -10,10 +9,6 @@ const start = async () => {
   await connectToDatabase();
   await redis.connect();
   await graphClient.initialize();
-
-  if (process.env.NODE_ENV === 'test') {
-    await insertMockData();
-  }
 
   app.listen(PORT, () => {
     logger.info(`Server running on port ${PORT}`);

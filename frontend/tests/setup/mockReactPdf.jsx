ccebbe84d5f7f92a454fbbda989bd927a49082ce
shdewz <shdewz@gmail.com>
2025-10-29 20:13:04 +0200

    Remove unnecessary test files

--- frontend/tests/setup/mockReactPdf.jsx
+++
@@ -1,9 +0,0 @@
-import { vi } from 'vitest';
-
-vi.mock('react-pdf', () => {
-  return {
-    Document: ({ children }) => <div data-testid="document">{children}</div>,
-    Page: () => <div data-testid="page">PDF Page</div>,
-    pdfjs: { GlobalWorkerOptions: { workerSrc: '' } },
-  };
-});

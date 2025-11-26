69face702503ecb1f6bc2877644b0c526016cde8
simo <49877847+saimouu@users.noreply.github.com>
2025-11-26 15:29:21 +0200

    Fix unicafe tests

--- frontend/tests/components/UnicafeMenu.unit.test.jsx
+++ frontend/tests/components/UnicafeMenu.unit.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { describe, test, expect, vi } from 'vitest';
+import userEvent from '@testing-library/user-event';
 
 import UnicafeMenu from '@/components/UnicafeMenu.jsx';
 import mockData from '../mocks/data/mockMenuData.json';
@@ -15,20 +16,31 @@ describe('UnicafeMenu unit tests', () => {
     render(<UnicafeMenu />);
   };
 
-  test('renders restaurant names correctly', async () => {
+  test('renders restaurant buttons correctly', async () => {
     setup();
 
     expect(await screen.findByText('Exactum')).toBeInTheDocument();
     expect(await screen.findByText('Chemicum')).toBeInTheDocument();
   });
 
-  test('renders menu items correctly', async () => {
+  test('renders exactum menu items correctly', async () => {
     setup();
 
     expect(
       await screen.findByText('Sweet potato and peanut soup')
     ).toBeInTheDocument();
+  });
+
+  test('chemicum menu can be accessed', async () => {
+    setup();
+    const user = userEvent.setup();
+
+    const chemicumButton = await screen.findByText('Chemicum');
+    await user.click(chemicumButton);
 
     expect(await screen.findByText('Berry quark dessert')).toBeInTheDocument();
+    expect(
+      await screen.findByText('Quinoa balls with paprika sause')
+    ).toBeInTheDocument();
   });
 });

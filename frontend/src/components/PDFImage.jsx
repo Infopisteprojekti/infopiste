97e6ab92a30d60a910b1a043e3620b3cfb18ca54
tiikerikakku <146967003+tiikerikakku@users.noreply.github.com>
2025-11-27 12:30:55 +0200

    fix pdf worker

--- frontend/src/components/PDFImage.jsx
+++ frontend/src/components/PDFImage.jsx
@@ -1,7 +1,7 @@
 import { Document, Page, pdfjs } from 'react-pdf';
 import { useState } from 'react';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const PDFImage = ({ form, preview }) => {
   const [allPages, setPages] = useState(null);

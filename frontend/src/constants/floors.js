2df80c01799eb7db8d7835039740bda441600372
jipeso <149601375+jipeso@users.noreply.github.com>
2025-11-29 00:16:05 +0200

    change to new floorplans and fix testing

--- frontend/src/constants/floors.js
+++ frontend/src/constants/floors.js
@@ -1,8 +1,10 @@
+import Floor0SVG from '@/assets/exactum-0.svg?react';
 import Floor1SVG from '@/assets/exactum-1.svg?react';
 import Floor2SVG from '@/assets/exactum-2.svg?react';
 import Floor3SVG from '@/assets/exactum-3.svg?react';
 
 const floors = [
+  { id: 0, label: '-1', svg: Floor0SVG },
   { id: 1, label: '1', svg: Floor1SVG },
   { id: 2, label: '2', svg: Floor2SVG },
   { id: 3, label: '3', svg: Floor3SVG },
